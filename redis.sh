c2bd50c4a453401d2105a68fc20b9e995ad129d8
heman chandra <hemanchandra701@gmail.com>
2025-10-05 21:57:33 +0530

    heman

--- redis.sh
+++ redis.sh
@@ -41,7 +41,7 @@ Validation $? "Enable Redis:7"
 dnf install redis -y &>>$Logs_File
 Validation $? "Install Redis"
 
-sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/c protected-mode no' /etc/redis/redis.conf &>>$Logs_File
+sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf &>>$Logs_File
 Validation $? "Redis Config to Public"
 
 systemctl enable redis &>>$Logs_File
